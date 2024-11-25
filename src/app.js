3ee7375de9154ede292ca9cb0d2b1fba70bdcf70
JonathanM1206 <jonathanmoesses@gmail.com>
2024-11-25 21:20:20 +0000

    Name Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
+/* eslint-disable 
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+*/
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extenciones = [".com", ".net", ".us", ".io"];
+let total = "";
 
-window.onload = function() {
+window.onload = function generador() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  total = "";
+  for (let j = 0; j < pronoun.length; j++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let y = 0; y < noun.length; y++) {
+        for (let z = 0; z < extenciones.length; z++) {
+          total += `<p>${pronoun[j]}${adj[x]}${noun[y]}${extenciones[z]}</p>`;
+          console.log(`${pronoun[j]}${adj[x]}${noun[y]}${extenciones[z]}`);
+        }
+      }
+    }
+  }
+  document.getElementById("nombres").innerHTML = total;
 };
